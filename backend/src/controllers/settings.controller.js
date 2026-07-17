fe18b46c1b87be039f48c191f60ad66920a1cca7
Sundar <sundareswarans151@gmail.com>
2026-07-17 14:04:17 +0530

    settings error

--- backend/src/controllers/settings.controller.js
+++ backend/src/controllers/settings.controller.js
@@ -91,23 +91,3 @@ module.exports = {
   deleteSetting,
   getDbStatus
 };
-
-const settingsService = require('../services/settings.service');
-const { asyncHandler } = require('../middleware/errorHandler');
-const { success } = require('../utils/response');
-
-const getSettings = asyncHandler(async (req, res) => {
-  const settings = await settingsService.getAllSettings();
-  success(res, settings, 'Settings retrieved successfully');
-});
-
-const updateSettings = asyncHandler(async (req, res) => {
-  const { settings, group } = req.body;
-  await settingsService.saveSettings(settings, group);
-  success(res, null, 'Settings updated successfully');
-});
-
-module.exports = {
-  getSettings,
-  updateSettings
-};
